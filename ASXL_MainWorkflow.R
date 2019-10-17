3c3129826e6aece7f7d33429dbadd66ce4baffbb
Theresa Lusardi <lusardi@ohsu.edu>
2019-10-17 11:42:44 -0700

    Updating - QCImport works in loop

--- ASXL_MainWorkflow.R
+++ ASXL_MainWorkflow.R
@@ -6,10 +6,10 @@ ExploreBiotypes = FALSE
 local = FALSE
 message(sprintf("local = %s", local))
 if (local) {
-  directory = list(code = "/Users/lusardi/Documents/CEDAR/Projects/3.ASXL_mutant/analysis/code",
+  dir = list(code = "/Users/lusardi/Documents/CEDAR/Projects/3.ASXL_mutant/analysis/code",
                    results = "/Users/lusardi/Documents/CEDAR/Projects/3.ASXL_mutant/analysis/results")
 } else {
-  directory = list(code = "/home/groups/CEDAR/lusardi/asxl/asxl1",
+  dir  = list(code = "/home/groups/CEDAR/lusardi/asxl/asxl1",
                    results = "/home/groups/CEDAR/braun/seurat_obj/test_rmd")
 }
 
@@ -18,40 +18,53 @@ library(data.table)
 library(rmarkdown)
 library(Seurat)
 
+processDate = Sys.Date()
+
 ### Summarize CellRanger Output Metrics
 print("CellRangerSummary")
 if (CellRangerSummary) {
   rmarkdown::render("0.AggregateCellRangerStats.Rmd",
-                    output_file = sprintf("%s/0.AggregateCellRangerStats_%s.html", directory$results, Sys.Date()))
+                    output_file = sprintf("%s/0.AggregateCellRangerStats_%s.html", dir$results, processDate))
 }
 
 ### Explore influence of normalization and scaling on HTO and ADT outputs
 print("AntibodyExplore chunk")
 if (AntibodyExplore) {
   rmarkdown::render("1a.EvalMultiSeqDemux.Rmd",
-                    output_file = sprintf("%s/1a.EvalMultiSeqCDemux_%s.html", directory$results, Sys.Date()))
+                    output_file = sprintf("%s/1a.EvalMultiSeqCDemux_%s.html", dir$results, processDate))
 }
 
 ### Per-run QC and initialization
 print("QCInit chunk")
-# List of runs that will be processed
 if (QCInit) {
+  # List of runs that will be processed
   runs2process = c("wk04_mpssr_wt", "wk04_mpssr_mut", 
                    "wk12_mpssr_wt", "wk12_mpssr_mut",
                    "wk36_mpssr_wt", "wk36_mpssr_mut", "wk36_novo_wt", "wk36_novo_mut")
   ADTskip = c("wk04_mpssr_mut", "wk12_mpssr_wt", "wk12_mpssr_mut", "wk36_mpssr_mut")
-  baseDir = "/home/groups/CEDAR/braun"
-  sourceDir = "cellranger"
-  destDir = paste("1.SeuratObj_TEST", Sys.Date(), sep = "_")
-  
-  for (myrun in runs2process[1]) { 
-    print(sprintf("Processing %s", myrun))
+  sourceDir = "/home/groups/CEDAR/braun/cellranger"
+
+  # Set up destination directory
+  baseDir = "/home/groups/CEDAR/braun/seurat_obj"
+  destDir = sprintf("%s/1.RunQC_%s", baseDir, processDate)
+  # If destDir does not exist, create it
+  if (!dir.exists(destDir)) {
+      dir.create(destDir)
+    if (!dir.exists(destDir)) {
+        message(sprintf("ERROR: could not create directory %s", destDir))
+        knitr::knit_exit()
+    }
+  }
+
+  for (myrun in runs2process) { 
+    print(sprintf("Processing input run %s", myrun))
     calcADT = TRUE
     if (myrun %chin% ADTskip) { calcADT = FALSE }
-    rmarkdown::render(paste(directory$code, "1.RunQC.Rmd", sep = "/"),
-                      output_file = sprintf("%s/1.%s_%s.html", directory$results, myrun, Sys.Date()),
+    print(sprintf("Processing script %s", paste(dir$code, "1.RunQC.Rmd", sep = "/")))
+    print(dir$code)
+    rmarkdown::render(paste(dir$code, "1.RunQC.Rmd", sep = "/"),
+                      output_file = sprintf("%s/1.%s_%s.html", destDir, myrun, processDate),
                       params = list(run2process = myrun,
-                                    baseDir = baseDir,
                                     sourceDir = sourceDir,
                                     destDir = destDir,
                                     file2save = myrun,
@@ -69,7 +82,7 @@ if (SCTxform) {
   for (myrun in runs2process[3:8]) { 
     print(sprintf("Processing %s", myrun))
     rmarkdown::render("/Users/lusardi/Documents/CEDAR/Projects/3.ASXL_mutant/analysis/code/2a.SCTransform.Rmd",
-                      output_file = sprintf("../results/2a.SCTxform_%s_%s.html", myrun, Sys.Date()), 
+                      output_file = sprintf("../results/2a.SCTxform_%s_%s.html", myrun, processDate), 
                       params = list(run2process = myrun,
                                     file2save = myrun,
                                     saveObj = TRUE))
